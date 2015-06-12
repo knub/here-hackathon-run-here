c69140f75ca1bd1757830ce96718fd4d990fab57
Stefan Bunk <stefan.bunk@googlemail.com>
2015-06-12 12:22:37 +0200

    Add models.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,44 @@
 $(document).ready(function() {
-	alert("I am ready");
 
     initMap();
-});
 
+
+
+
+
+
+
+
+
+	var Day = Backbone.Model.extend({
+		defaults: {
+			"weatherForecast": "sunny"
+		}
+
+	});
+	var TrainingPlan = Backbone.Collection.extend({
+		model: Day
+	});
+
+
+	var trainingPlan = new TrainingPlan([
+		new Day({
+			title: "Day 1",
+			staticRouteUrl: "http://dummyimage.com/200x200/dddddd/000000.png",
+			length: "4 km"
+		}),
+		new Day({
+			title: "Day 3",
+			staticRouteUrl: "http://dummyimage.com/200x200/dddddd/000000.png",
+			length: "7 km"
+		}),
+		new Day({
+			title: "Day 7",
+			staticRouteUrl: "http://dummyimage.com/200x200/dddddd/000000.png",
+			length: "8 km"
+		})
+	]);
+});
 /**
  * This function will be called once the Routing REST API provides a response
  * @param  {Object} result          A JSONP object representing the calculated route
