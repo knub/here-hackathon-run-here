6f2cb3017aeaf9b0c9e4c3586dc8d5f8528c3901
Philipp Otto <philippotto.po@gmail.com>
2015-06-12 12:22:39 +0200

    move js code from index to main.js

--- js/main.js
+++ js/main.js
@@ -3,3 +3,17 @@ $(document).ready(function() {
 
     initMap();
 });
+
+/**
+ * This function will be called once the Routing REST API provides a response
+ * @param  {Object} result          A JSONP object representing the calculated route
+ *
+ * see: http://developer.here.com/rest-apis/documentation/routing/topics/resource-type-calculate-route.html
+ */
+function onSuccess(result) {
+  addRoute(result);
+}
+
+function onError(error) {
+  alert('Ooops!');
+}
